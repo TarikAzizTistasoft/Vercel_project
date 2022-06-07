d769ed14089293f01bc45beceb5b01ed35f1784d
TarikAzizTistasoft <106754474+TarikAzizTistasoft@users.noreply.github.com>
2022-06-07 06:36:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from seoanalyzer import analyze
+@app.get("/", tags=["Root"])
+async def read_root():
+  return { 
+    "message": "Welcome to my notes application, use the /docs route to proceed"
+   }
